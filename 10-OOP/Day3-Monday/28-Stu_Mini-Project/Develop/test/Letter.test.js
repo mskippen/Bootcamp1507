a12ea71450f996fe1b605f7063e2597df7972b01
Sandes <sandeshpathak0112@gmail.com>
2021-05-03 16:45:16 +1000

    folders fixed

--- 10-OOP/Day3-Monday/28-Stu_Mini-Project/Develop/test/Letter.test.js
+++ 10-OOP/Day3-Monday/28-Stu_Mini-Project/Develop/test/Letter.test.js
@@ -19,6 +19,40 @@ describe("Letter class", () => {
     });
   });
 
+  describe("visible", () => {
+    it("Characters that are digits or letters should not be visible", () => {
+      expect(new Letter("j").visible).toBe(false);
+    });
+    
+    it("Correct guess makes character visible", () => {
+      const letter = new Letter("j");
+      const visible = letter.visible;
+      
+      letter.guess("j");
+
+      expect(visible).toBe(false);       // letter was not originally visible
+      expect(letter.visible).toBe(true); // after correct guess letter should be visible
+    });
+
+    it("Incorrect guess does not make character visible", () => {
+      const letter = new Letter("j");
+      const visible = letter.visible;
+      
+      letter.guess("l");
+
+      expect(visible).toBe(false);        // letter was not originally visible
+      expect(letter.visible).toBe(false); // after incorrect guess letter should still not be visible
+    });
+
+    it("toString returns character when character is visible", () => {
+      const letter = new Letter("j");
+      
+      letter.guess("j");
+
+      expect(letter.toString()).toBe("j");
+    });
+  });
+
   describe("getSolution", () => {
     it("returns character", () => {
       expect(new Letter("l").getSolution()).toBe("l");
